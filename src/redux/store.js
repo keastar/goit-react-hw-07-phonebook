be347f209a74f008bbfb52a827aeb9bd7fb55a82
keastar <keastar1986@gmail.com>
2023-10-09 18:37:22 +0300

    7HW!
    7HW!

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,25 +7,25 @@ import { filtersReducer } from './filtersSlice';
 // const myReducer = createReducer();
 const enhancer = devToolsEnhancer();
 
-function myMiddleware1(store) {
-  return function (next) {
-    return function (action) {
-      console.log('myMiddleware1', action);
-      next(action);
-    };
-  };
-}
+// function myMiddleware1(store) {
+//   return function (next) {
+//     return function (action) {
+//       console.log('myMiddleware1', action);
+//       next(action);
+//     };
+//   };
+// }
 export const store = configureStore(
   {
     reducer: {
       contacts: contactsReducer,
       filters: filtersReducer,
     },
-    middleware(getDefaultMiddleware) {
-      const defaultMd = getDefaultMiddleware();
-      console.log(defaultMd);
-      return [...defaultMd, myMiddleware1];
-    },
+    // middleware(getDefaultMiddleware) {
+    //   const defaultMd = getDefaultMiddleware();
+    //   console.log(defaultMd);
+    //   return [...defaultMd, myMiddleware1];
+    // },
   },
   enhancer
 );
